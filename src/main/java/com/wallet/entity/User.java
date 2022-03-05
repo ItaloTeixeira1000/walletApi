6bb87a723687b93eb3633c9bb5c91435c843b610
Italo Teixeira <teixeiraitalo600@gmail.com>
2022-03-05 19:56:00 -0300

    Finalizado repositório do Usuário

---
+++ src/main/java/com/wallet/entity/User.java
@@ -0,0 +1,35 @@
+package com.wallet.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4853559697919162240L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String email;
+
+}
